56f9f2075a5068da1e96275acd8292970879aba4
Romacardozx <roman.alejandro.cardozo@gmail.com>
2021-09-06 20:11:08 -0300

    Models

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -3,10 +3,38 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
+  //?DEFINO LA TABLA DE BREED EN LA BASE DE DATOS 
+  //?DEFINING BREED TABLE AT DB
   sequelize.define('dog', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    height: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    life_span: {
+      type: DataTypes.STRING,
+    },
+    image: {
+      type: DataTypes.STRING,
+    }
+  },
+    { timestamps: false 
   });
 };
+  //?DEFINO LA TABLA DE TEMPERAMENTO EN LA BASE DE DATOS
+  //?DEFINING TEMPERAMENT TABLE AT DB
+  sequelize.define('temperament', {
+  name: {
+    type: DataTypes.STRING
+  },
+},
+  { timestamps: false 
+});
+
