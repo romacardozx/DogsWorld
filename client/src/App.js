6f4ec74e786aabd8e660a590beb00bab1c05b4df
Romacardozx <roman.alejandro.cardozo@gmail.com>
2021-09-07 21:38:46 -0300

    modularizacion del front

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
-import './App.css';
+import React, { useState } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Home from './views/home/Home'
+import { Nav } from './components/nav/nav'
+import Breed from './views/breed/Breed'
+import Landing from './views/landing/landing'
+import CreateBreed from './views/createBreed/CreateBreed'
 
 function App() {
+  const [input, setInput] = useState('')
+
   return (
     <div className="App">
-      <h1>Henry Dogs</h1>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/">
+          <Nav setInput={setInput} input={input} />
+          <Route
+            exact
+            path="/home"
+            render={() => <Home setInput={setInput} input={input} />}
+          />
+          <Route exact path="/create" component={CreateBreed} />
+          <Route
+            exact
+            path="/detail/:id"
+            render={({ match }) => <Breed id={match.params.id}></Breed>}
+          ></Route>
+        </Route>
+      </Switch>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ client/src/views/landing/landing.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import style from './landing.module.css'
+import { useHistory } from 'react-router'
+
+//*Basic Landing Page with button to push user to /home
+function LandingPage() {
+  const history = useHistory()
+  return (
+    <div className={style.landing}>
+      <div className={style.msgContainer}>
+        <p className={style.title}>Welcome to the Dogs App</p>
+        <button className={style.button} onClick={() => history.push('/home')}>
+          Enter
+        </button>
+      </div>
+    </div>
+  )
+}
+export default LandingPage
